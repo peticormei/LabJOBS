d29976f6302fe8acd97d50044c2b81e99d4660bd
Filipe <peticormei@gmail.com>
2016-04-22 18:14:00 +0000

    segunda versao do batalha naval

--- E6 - Batalha Naval/filipe_peticormei_E6.py
+++ E6 - Batalha Naval/filipe_peticormei_E6.py
@@ -6,6 +6,24 @@ fileOut = open(sys.argv[2], mode = 'w')
 tab = []
 points = []
 count = 0
+t = 0
+
+def l_boats(tab, m, n):
+    l = []
+    b = []
+    for i in range(m):
+        if b != []:
+            l.append(b)
+            b = []
+        for j in range(n):
+            if tab[i][j] != '.':
+                b.append(tab[i][j])
+            elif b != []:
+                l.append(b)
+                b = []
+            else:
+                pass
+    return l
 
 size_tab = list(map(int, fileIn.readline().split()))
 if size_tab[0] >= 1 and size_tab[0] <= 100:
@@ -21,10 +39,13 @@ if size_tab[0] >= 1 and size_tab[0] <= 100:
         for p in points:
             if tab[p[0]-1][p[1]-1] == '#':
                 tab[p[0]-1][p[1]-1] = 'X'
-        for i in range(size_tab[0]):
-            for j in range(size_tab[1]):
-                if tab[i][j] == 'X':
-                    count +=1
+        for b in l_boats(tab, size_tab[0], size_tab[1]):
+            for p in b:
+                if p == 'X':
+                    t += 1
+            if t == len(b):
+                t = 0
+                count += 1
         fileOut.write(str(count)+'\n')
     else:
         fileOut.write('O numero de M nao respeitou a restricao')
